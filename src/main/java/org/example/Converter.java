9e229c6affbef8a0577163f3feea5e025a150a2c
Hector Prado <Pradohm@gmail.com>
2024-10-29 22:40:58 -0300

    Primeiro commit

---
+++ src/main/java/org/example/Converter.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Converter {
+
+    static int valor ;
+    static double valorDouble = valor;
+
+    public static void digitarValores(int valor) {
+        System.out.println("O valor em int é: " + valor +" e o valor em double é: " + valorDouble);
+    }
+
+
+}
+
