df2a45a920d73d36832dd39180fe8650cf03a2cc
Hector Prado <Pradohm@gmail.com>
2024-10-30 22:14:14 -0300

    Arrumando a tarefa

--- src/main/java/org/example/Converter.java
+++ src/main/java/org/example/Converter.java
@@ -3,7 +3,7 @@ package org.example;
 public class Converter {
 
     static int valor ;
-    static Double valorDouble = Double.valueOf(valor);
+    static Double valorDouble = (double) valor;
 
     public static void digitarValores(int valor) {
         System.out.println("O valor em int é: " + valor +" e o valor em double é: " + valorDouble);
